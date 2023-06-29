e3d3bb1fb47105b2675ef4d3bdaa188da4e57dc2
Smart Factory Believers <128001053+SmartFactoryBelievers@users.noreply.github.com>
2023-06-29 18:22:41 -0500

    Update Project06_LightActivatedDriving.py

--- Project06_LightActivatedDriving.py
+++ Project06_LightActivatedDriving.py
@@ -1,116 +1,92 @@
-# Project 6
-# Learning to program, writing functions, using motor control outputs, adding loop complexity
-# Build the the Project 6 circuit and have the rover be controlled by ambient light
-# Turn down the ligth and point a flashlight at the rover to direct it
-
-#Challenge 1
-# Try changing the drive functions to switch the driving directions
-
-#Challenge 2
-# Add new drive functions to change its light seeking spin pattern
-
-#Challege 3
-# Add the 100 Ohm resistor in series with the photoresistor to increase light sensitivity
-
-#Challege 4
-# With the modulo operator, have the rover alternate left or right spins in light searching
-
-#Challenge 5
-# After a certain amount of time, have the rover spin to look for light
-
-#Importing libraries
-# Here we want the time and sleep for timing and GPIO for the Pi's pins
-import time
-from time import sleep
-import RPi.GPIO as GPIO
-
-#Let's define variables so we can use them later
-Left_Forward_Pin = 35 #the internal Pi pin number that goes to snap 1
-Left_Backward_Pin = 31 #the internal Pi pin number that goes to snap 2
-Right_Forward_Pin = 26 #the internal Pi pin number that goes to snap 3
-Right_Backward_Pin = 21 #the internal Pi pin number that goes to snap 4
-Photo_Pin = 18 #the internal Pi pin number that goes to snap 6
-#Here we can define the timing variables for the driving functions, in seconds
-Forward_Time = 2
-Backward_Time = 1
-Left_Turn_Time = 0.5
-Right_Turn_Time = 0.5
-Wait_Time = 1
-
-#Setting up our pins
-GPIO.setmode(GPIO.BOARD)
-#Our output pins, start off
+# Proyecto 6 
+# Aprendiendo a programar, escribir funciones, usar salidas de control de motor, y agregar un complejo bucle
+# Construya el Proyecto 6 circuito y hacer que el rover sea controlado por la luz ambiental  # Baje la luz y apunte una linterna al rover para dirigirlo 
+#Desafío 1 
+# Trate de cambiar las funciones de conducción para cambiar las direcciones de conducción 
+#Desafío 2 
+# Agregue nuevas funciones de manejo para cambiar su patrón de giro de búsqueda de luz 
+#Desafío 3 
+# Agregue la resistencia de 100 Ohm en serie con la fotorresistencia para aumentar la sensibilidad a la luz 
+#Desafío 4 
+# Con el operador de módulo, haga que el rover alterne los giros a la izquierda o a la derecha en la búsqueda de la luz 
+#Desafío 5 
+# Después de una cierta cantidad de tiempo, haz que el rover gire para buscar luz. 
+#Importing libraries 
+# Aquí queremos la función sleep para el tiempo y GPIO para el pin de Pi import time 
+from time import sleep 
+import RPi.GPIO as GPIO 
+#Definamos variables para poder usarlas más tarde 
+Left_Forward_Pin = 36 #el número de pin interno de Pi que se ajusta a 1 Left_Backward_Pin = 11 #el número de pin interno de Pi que se ajusta a 2 Right_Forward_Pin = 12 #el número de pin interno de Pi que se ajusta a 3 Right_Backward_Pin = 35 #el número de pin interno de Pi que se ajusta a 4 Photo_Pin = 38 #el número de pin interno de Pi que se ajusta a 5 
+#Aquí podemos definir las variables de tiempo para las funciones de conducción, en segundos Forward_Time = 2 
+Backward_Time = 1 
+Left_Turn_Time = 0.5 
+Right_Turn_Time = 0.5 
+Wait_Time = 1 
+#Configurando nuestros pines
+GPIO.setmode(GPIO.BOARD) 
+#Nuestros pines de salida, empiezan apagado  
 GPIO.setup(Left_Forward_Pin, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(Left_Backward_Pin, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(Right_Forward_Pin, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(Right_Backward_Pin, GPIO.OUT, initial=GPIO.LOW)
-#Our input pin from the button
-GPIO.setup(Photo_Pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-#Let's write some driving functions we can use later
-def drive_forward(time):
-GPIO.output(Left_Forward_Pin, GPIO.HIGH) #Left motor forward
-GPIO.output(Right_Forward_Pin, GPIO.HIGH) #Right motor forward
-sleep(time)
-GPIO.output(Left_Forward_Pin, GPIO.LOW) #Left motor off
-GPIO.output(Right_Forward_Pin, GPIO.LOW) #Right motor off
-print('forward')
-sleep(1)
-def drive_left_turn(time):
-GPIO.output(Left_Backward_Pin, GPIO.HIGH) #Left motor backward
-GPIO.output(Right_Forward_Pin, GPIO.HIGH) #Right motor forward
-sleep(time)
-GPIO.output(Left_Backward_Pin, GPIO.LOW) #Left motor off
-GPIO.output(Right_Forward_Pin, GPIO.LOW) #Right motor off
-print('left turn')
-sleep(1)
-def drive_right_turn(time):
-GPIO.output(Left_Forward_Pin, GPIO.HIGH) #Left motor forward
-GPIO.output(Right_Backward_Pin, GPIO.HIGH) #Right motor backward
-sleep(time)
-GPIO.output(Left_Forward_Pin, GPIO.LOW) #Left motor off
-GPIO.output(Right_Backward_Pin, GPIO.LOW) #Right motor off
-print('right turn')
-sleep(1)
-def drive_backward(time):
-GPIO.output(Left_Backward_Pin, GPIO.HIGH) #Left motor backward
-GPIO.output(Right_Backward_Pin, GPIO.HIGH) #Right motor backward
-sleep(time)
-GPIO.output(Left_Backward_Pin, GPIO.LOW) #Left motor off
-GPIO.output(Right_Backward_Pin, GPIO.LOW) #Right motor off
-print('backward')
-sleep(1)
-
-# For challenge 4, we will use a dummy variable to help with modulo operator
-count = 0
-# Replace the True with the modulo operator statement as %, which means remainder in division
-# So modulo 2 keeps track of odd and even presses since even divided by 2 has remainder of 0
-# To use this as a logical, let's try count % 2 == 0
-# For challenge 5, we will set a maximum light search time for the loop
-Max_Search_Time = 4 #seconds
-# If the rover has not found light by then, we can get out of the loop with a break statement
-# break exits the innermost loop and allows the rover to return to the first sleep command
-while True: # Continuous outer while loop
-sleep(0.25)
-count = count + 1 # Increment the counter for the modulo
-# If the phototransistor detects enough light, drive towards it
-if GPIO.input(Photo_Pin):
-# For challenges 1 and 2, change driving instructions here
-drive_forward(Forward_Time)
-# If there's not enough light, let's look for it by spinning the rover
-else:
-# For challenge 5, we can use the timer function to control the light seach
-Start_Time = time.time()
-while not(GPIO.input(Photo_Pin)):
-Elapsed_Time = round(time.time() - Start_Time,2)
-print('Not enough light, searching for more')
-if True: # Try changing the True to a comparitive (<) between
-# Elapsed_Time and Max_Search_Time for challenge 5
-if True: # Try changing the True to the modulo for challenge 4
-drive_left_turn(Left_Turn_Time)
-sleep(Wait_Time)
-else: # For challenge 4, modulo uses these drive commands on odd loops
-drive_right_turn(Right_Turn_Time)
-sleep(Wait_Time)
-else:
-break # Exits the loop after Max Search Time exceeded
+Copyright © Deloitte Development LLC 2022. All Rights Reserved. Developed in collaboration with the National Math + Science Initiative. 
+GPIO.setup(Left_Backward_Pin, GPIO.OUT, initial=GPIO.LOW) 
+GPIO.setup(Right_Forward_Pin, GPIO.OUT, initial=GPIO.LOW) 
+GPIO.setup(Right_Backward_Pin, GPIO.OUT, initial=GPIO.LOW) 
+#Nuestro pin de entrada desde el botón 
+GPIO.setup(Photo_Pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
+#Escribamos algunas funciones de conducción que podamos usar más tarde
+def drive_forward(time):  
+ GPIO.output(Left_Forward_Pin, GPIO.HIGH) #Motor izquierdo adelante  GPIO.output(Right_Forward_Pin, GPIO.HIGH) #Motor derecho adelante  sleep(time) 
+ GPIO.output(Left_Forward_Pin, GPIO.LOW) #motor izquierdo apagado 
+ GPIO.output(Right_Forward_Pin, GPIO.LOW) #motor derecho apagado 
+ print('forward') 
+ sleep(1) 
+def drive_left_turn(time): 
+ GPIO.output(Left_Backward_Pin, GPIO.HIGH) #Motor izquierdo hacia atrás  GPIO.output(Right_Forward_Pin, GPIO.HIGH) #Motor derecho adelante  sleep(time) 
+ GPIO.output(Left_Backward_Pin, GPIO.LOW) #motor izquierdo apagado 
+ GPIO.output(Right_Forward_Pin, GPIO.LOW) #motor derecho apagado 
+ print('left turn') 
+ sleep(1) 
+  
+def drive_right_turn(time): 
+ GPIO.output(Left_Forward_Pin, GPIO.HIGH) #Motor izquierdo adelante  GPIO.output(Right_Backward_Pin, GPIO.HIGH) #Motor derecho hacia atrás sleep(time) 
+ GPIO.output(Left_Forward_Pin, GPIO.LOW) #motor izquierdo apagado 
+ GPIO.output(Right_Backward_Pin, GPIO.LOW) #motor derecho apagado 
+ print('right turn') 
+ sleep(1) 
+def drive_backward(time): 
+ GPIO.output(Left_Backward_Pin, GPIO.HIGH) #Motor izquierdo hacia atrás  GPIO.output(Right_Backward_Pin, GPIO.HIGH) #Motor derecho hacia atrás  sleep(time) 
+ GPIO.output(Left_Backward_Pin, GPIO.LOW) #motor izquierdo apagado 
+ GPIO.output(Right_Backward_Pin, GPIO.LOW) #motor derecho apagado 
+ print('backward') 
+ sleep(1) 
+# Para desafío 4, usaremos una variable ¨dummy¨ para ayudar con el operador de módulo count = 0 
+# Reemplace las declaraciones True con la sentencia del operador módulo cómo %, que significa resto en división 
+Copyright © Deloitte Development LLC 2022. All Rights Reserved. Developed in collaboration with the National Math + Science Initiative. 
+# Entonces, el módulo 2 realiza las pulsaciones pares e impares por dividiendo por 2 tiene un resto de 0 
+# Para usar esto como una lógica, intentemos count % 2 == 0  
+# Para desafío 5, estableceremos un tiempo máximo de búsqueda de luz para el bucle Max_Search_Time = 4 #segundos 
+# Si el rover no ha encontrado luz por un tiempo, podemos salir del bucle con una declaración de interrupción
+# break sale del bucle más interno y permite que el móvil regrese al primer mandato de sleep 
+while True: # Externo continuo While bucle
+ sleep(0.25) 
+ count = count + 1 # Incrementa el contador para el módulo
+  
+ # Si el fototransistor detecta suficiente luz, conduzca hacia el luz  if GPIO.input(Photo_Pin): 
+ # Para desafíos 1 y 2, cambie las instrucciones de conducción aquí drive_forward(Forward_Time) 
+  
+ # Si no hay suficiente luz, gira el rover
+ else: 
+ # Para desafío 5, podemos usar la función de temporizador para controlar la búsqueda de luz  Start_Time = time.time() 
+ while not(GPIO.input(Photo_Pin)): 
+ Elapsed_Time = round(time.time() - Start_Time,2) 
+ print('Not enough light, searching for more') 
+  
+ if Elapsed_Time < Max_Search_Time: # Trate cambiando la declaración True a un comparativo (<) entre
+ # Elapsed_Time y Max_Search_Time para desafío 5   
+ if count % 2 == 0: # Trata cambiando el True declaración al módulo para desafío 4 
+ drive_left_turn(Left_Turn_Time) 
+sleep(Wait_Time) 
+ else: # Para desafío 4, módulo usa estos mandatos de manejo en bucles impares 
+ drive_right_turn(Right_Turn_Time) 
+ sleep(Wait_Time) 
+ else: 
+ break # Salga del bucle después Max Search Time termina
