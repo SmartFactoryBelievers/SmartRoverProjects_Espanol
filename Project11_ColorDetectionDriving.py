85e8ab12ca0bb1a646108466c6dcae94b309bf4f
Smart Factory Believers <128001053+SmartFactoryBelievers@users.noreply.github.com>
2023-06-29 18:24:02 -0500

    Update Project11_ColorDetectionDriving.py

--- Project11_ColorDetectionDriving.py
+++ Project11_ColorDetectionDriving.py
@@ -1,155 +1,125 @@
-# Project 11
-# Using the Pi camera to capture and analyze the color profile of objects
-# Build the the Project 11 circuit and drive the rover according to colored signs
-
-#Challenge 1
-# Try changing the colors associated with the driving commands, like flipping red and green
-
-#Challenge 2
-# Try adding a modulo operator to alternate between left and right turns on blue signs
-
-#Challege 3
-# Try setting the drive time variables based on the promince of the color from the argmax
-
-#Challenge 4
-# Try adding a memory variable for the last color identified and dictate driving
-# based on the pattern, like Red then Green
-
-#Importing libraries
-# Here we want sleep for timing, GPIO for the Pi's pins, & picamera for the Pi's camera
-from time import sleep
-import RPi.GPIO as GPIO
-from picamera import PiCamera
-# Numpy is a great numerical tools package to help with the math required
-import numpy as np
-
-#Let's define variables so we can use them later
-Left_Forward_Pin = 35 #the internal Pi pin number that goes to snap 1
-Left_Backward_Pin = 31 #the internal Pi pin number that goes to snap 2
-Right_Forward_Pin = 26 #the internal Pi pin number that goes to snap 3
-Right_Backward_Pin = 21 #the internal Pi pin number that goes to snap 4
-Button_Pin = 18 #the internal Pi pin number that goes to snap 6
-#Here we can define the timing variables for the driving functions, in seconds
-Forward_Time = 2
-Backward_Time = 1
-Left_Turn_Time = 0.5
-Right_Turn_Time = 0.5
-Wait_Time = 1
-#Setting up our pins
-GPIO.setmode(GPIO.BOARD)
-#Our output pins, start off
+# Proyecto 11 
+# Usando la cámara del Pi para capturar y analizar los colores de los objetos # Construya el Proyecto 11 circuito y conduce el rover de acuerdo con las señales de colores 
+#Desafío 1 
+# Trate de cambiar los colores asociados con los mandatos de manejo, como cambiar el rojo y el verde 
+#Desafío 2 
+# Trate de agregar un operador de módulo para alternar entre giros a la izquierda y a la derecha en señales azules
+#Desafío 3 
+# Trate de configurar las variables de tiempo de manejo en función de la prominencia del color del argmax 
+#Desafío 4 
+# Trate de agregar una variable de memoria para el último color identificado y conducir siguiendo el patrón, como rojo y luego verde 
+#Importing libraries 
+# Aquí queremos sleep para el tiempo, GPIO para los pines de Pi y picamera para la cámara de Pi 
+from time import sleep 
+import RPi.GPIO as GPIO 
+from picamera import PiCamera 
+# Numpy es un gran paquete de herramientas numéricas para ayudar con las matemáticas import numpy as np 
+#Definamos variables para poder usarlas más tarde 
+Left_Forward_Pin = 36 #el número de pin interno de Pi que se ajusta a 1 Left_Backward_Pin = 11 #el número de pin interno de Pi que se ajusta a 2 Right_Forward_Pin = 12 #el número de pin interno de Pi que se ajusta a 3 Right_Backward_Pin = 35 #el número de pin interno de Pi que se ajusta a 4 Button_Pin = 38 #el número de pin interno de Pi que se ajusta a 5 
+#Aquí podemos definir las variables de tiempo para las funciones de conducción, en segundos Forward_Time = 2 
+Backward_Time = 1 
+Left_Turn_Time = 0.5 
+Right_Turn_Time = 0.5 
+Wait_Time = 1 
+#Configurando nuestros pines 
+GPIO.setmode(GPIO.BOARD) 
+#Nuestros pines de salida, empiezan apagado  
 GPIO.setup(Left_Forward_Pin, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(Left_Backward_Pin, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(Right_Forward_Pin, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(Right_Backward_Pin, GPIO.OUT, initial=GPIO.LOW)
-#Our input pins, start down
-GPIO.setup(Button_Pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-#Let's write some driving functions we can use later
-def drive_forward(time):
-GPIO.output(Left_Forward_Pin, GPIO.HIGH) #Left motor forward
-GPIO.output(Right_Forward_Pin, GPIO.HIGH) #Right motor forward
-sleep(time)
-GPIO.output(Left_Forward_Pin, GPIO.LOW) #Left motor off
-GPIO.output(Right_Forward_Pin, GPIO.LOW) #Right motor off
-print('forward')
-sleep(1)
-def drive_left_turn(time):
-GPIO.output(Left_Backward_Pin, GPIO.HIGH) #Left motor backward
-GPIO.output(Right_Forward_Pin, GPIO.HIGH) #Right motor forward
-sleep(time)
-GPIO.output(Left_Backward_Pin, GPIO.LOW) #Left motor off
-GPIO.output(Right_Forward_Pin, GPIO.LOW) #Right motor off
-print('left turn')
-sleep(1)
-def drive_right_turn(time):
-GPIO.output(Left_Forward_Pin, GPIO.HIGH) #Left motor forward
-GPIO.output(Right_Backward_Pin, GPIO.HIGH) #Right motor backward
-sleep(time)
-GPIO.output(Left_Forward_Pin, GPIO.LOW) #Left motor off
-GPIO.output(Right_Backward_Pin, GPIO.LOW) #Right motor off
-print('right turn')
-sleep(1)
-def drive_backward(time):
-GPIO.output(Left_Backward_Pin, GPIO.HIGH) #Left motor backward
-GPIO.output(Right_Backward_Pin, GPIO.HIGH) #Right motor backward
-sleep(time)
-GPIO.output(Left_Backward_Pin, GPIO.LOW) #Left motor off
-GPIO.output(Right_Backward_Pin, GPIO.LOW) #Right motor off
-print('backward')
-sleep(1)
-
-# For challenge 2, we will use a dummy variable to help with modulo operator
-count = 0
-# Replace the True with the modulo operator statement as %, which means remainder in division
-# So modulo 2 keeps track of odd and even presses since even divided by 2 has remainder of 0
-# To use this as a logical, let's try count % 2 == 0
-
-# For challenge 3, we will set the variable color intensity to scale the drive times
-# First, let's define it so we can use the code as is
-Color_Intensity = 1
-
-# Setting up camera for analysis and to emphasize colors
-camera = PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 30
-sleep(2) #let the cameraera settle
-camera.iso = 100
-camera.shutter_speed = camera.exposure_speed
-camera.exposure_mode = 'off'
-gain_set = camera.awb_gains
-camera.awb_mode = 'off'
-camera.awb_gains = gain_set
-
-# Prepping for image analysis and eliminating background Noise
-#Images are stored in a 3D array with each pixel having Red, Green, and Blue values
-Image = np.empty((640,480,3),dtype=np.uint8)
-Noise = np.empty((640,480,3),dtype=np.uint8)
-RGB_Text = ['Red','Green','Blue'] #Array for naming color
-# Let's remove the background 'Noise' colors to emphasis the object's color
-camera.capture(Noise,'rgb')
-Noise = Noise-np.mean(Noise)
-
-#Looping with different images to determine object colors
-print('Ready to take photo')
-while True:
-#Press the push button to capture an image
-if GPIO.input(Button_Pin) == True:
-sleep(2)
-print('Photo taken')
-camera.capture(Image,'rgb')
-RGB_Array = []
-# For each of red, green, and blue, calculate the most prominent color through means
-for col in range(0,3):
-RGB_Array.append(np.mean(Image[:,:,col]-np.mean(Image)-np.mean(Noise[:,:,col])))
-Color = RGB_Text[np.argmax(RGB_Array)]
-print(Color)
-
-# For challenge 3, let's compare the most prominent color to the second most
-# We can use this ratio to set the Color_Intensity variable
-# with max as np.max(RGB_Array) and mid as np.median(RGB_Array)
-# However, the color channels can be negative, so let's use a max to keep positive
-#Color_Intensity = np.max([? / ?, 2])
-
-# For challenge 4, let's look for a pattern like Red then Color
-# We can use an if statement to see if the Last_Color was Red
-# Replace this True with a logical to check, remember it's ==, not = here
-if True:
-# Activate motor controller outputs based on the determined object color
-if Color == 'Red': #Backward for Red object
-drive_backward(Backward_Time * Color_Intensity)
-sleep(Wait_Time)
-if Color == 'Green': #Forward for Green object
-drive_forward(Forward_Time * Color_Intensity)
-sleep(Wait_Time)
-if Color == 'Blue': #Turn for Blue object
-if True: # Try changing the True to the modulo for challenge 2
-drive_left_turn(Left_Turn_Time * Color_Intensity)
-else: # For challenge 2, modulo uses these drive commands on odd loops
-drive_right_turn(Right_Turn_Time * Color_Intensity)
-sleep(Wait_Time)
-count = count + 1 # Increment the counter for the modulo
-
-# For challenge 4, update Last_Color after outputs
-#Last_Color = Color
-print('Ready to take photo')
+Copyright © Deloitte Development LLC 2022. All Rights Reserved. Developed in collaboration with the National Math + Science Initiative. 
+GPIO.setup(Left_Backward_Pin, GPIO.OUT, initial=GPIO.LOW) 
+GPIO.setup(Right_Forward_Pin, GPIO.OUT, initial=GPIO.LOW) 
+GPIO.setup(Right_Backward_Pin, GPIO.OUT, initial=GPIO.LOW) 
+#Nuestros pines de entrada, empiezan abajo 
+GPIO.setup(Button_Pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
+#Escribamos algunas funciones de conducción que podamos usar más adelante 
+def drive_forward(time):  
+ GPIO.output(Left_Forward_Pin, GPIO.HIGH) #Motor izquierdo adelante GPIO.output(Right_Forward_Pin, GPIO.HIGH) #Motor derecho adelante  sleep(time) 
+ GPIO.output(Left_Forward_Pin, GPIO.LOW) #motor izquierdo apagado  
+ GPIO.output(Right_Forward_Pin, GPIO.LOW) #motor derecho apagado
+ print('forward') 
+ sleep(1) 
+def drive_left_turn(time): 
+ GPIO.output(Left_Backward_Pin, GPIO.HIGH) #Motor izquierdo hacia atrás  GPIO.output(Right_Forward_Pin, GPIO.HIGH) #Motor derecho adelante  sleep(time) 
+ GPIO.output(Left_Backward_Pin, GPIO.LOW) #motor izquierdo apagado  
+ GPIO.output(Right_Forward_Pin, GPIO.LOW) #motor derecho apagado 
+ print('left turn') 
+ sleep(1) 
+  
+def drive_right_turn(time): 
+ GPIO.output(Left_Forward_Pin, GPIO.HIGH) #Motor izquierdo adelante GPIO.output(Right_Backward_Pin, GPIO.HIGH) #Motor derecho hacia atrás  sleep(time) 
+ GPIO.output(Left_Forward_Pin, GPIO.LOW) #motor izquierdo apagado  
+ GPIO.output(Right_Backward_Pin, GPIO.LOW) #motor derecho apagado 
+ print('right turn') 
+ sleep(1) 
+def drive_backward(time): 
+ GPIO.output(Left_Backward_Pin, GPIO.HIGH) #Motor izquierdo hacia atrás  GPIO.output(Right_Backward_Pin, GPIO.HIGH) #Motor derecho hacia atrás  sleep(time) 
+ GPIO.output(Left_Backward_Pin, GPIO.LOW) #motor izquierdo apagado 
+ GPIO.output(Right_Backward_Pin, GPIO.LOW) #motor derecho apagado
+ print('backward') 
+ sleep(1) 
+  
+# Para desafío 2, usaremos una variable ¨dummy¨ para ayudar con el operador de módulo count = 0 
+# Reemplace la True declaraciones con la sentencia del operador módulo cómo %, que significa resto en división
+# Entonces, el módulo 2 realiza las pulsaciones pares e impares por dividiendo por 2 tiene un resto de 0
+Copyright © Deloitte Development LLC 2022. All Rights Reserved. Developed in collaboration with the National Math + Science Initiative. 
+# Para usar esto como una lógica, intentemos count % 2 == 0  
+# Para desafío 3, estableceremos la intensidad de color variable para escalar los tiempos de conducción
+Color_Intensity = 1 
+# Configuración de la cámara para el análisis y para enfatizar los colores 
+camera = PiCamera() 
+camera.resolution = (640, 480) 
+camera.framerate = 30 
+sleep(2) #deja que la cámara asiente 
+camera.iso = 100 
+camera.shutter_speed = camera.exposure_speed 
+camera.exposure_mode = 'off' 
+gain_set = camera.awb_gains 
+camera.awb_mode = 'off' 
+camera.awb_gains = gain_set 
+# Preparación para el análisis de imágenes y eliminación del ruido
+#Las imágenes se almacenan en una matriz 3D y cada píxel tiene valores de rojo, verde y azul Image = np.empty((640,480,3),dtype=np.uint8) 
+Noise = np.empty((640,480,3),dtype=np.uint8) 
+RGB_Text = ['Red','Green','Blue'] #Matriz para nombrar colores 
+# Quitando el ruido camera.capture(Noise,'rgb') 
+Noise = Noise-np.mean(Noise) 
+#Bucle con diferentes imágenes para determinar los colores de los objetos 
+print('Ready to take photo') 
+while True: 
+  
+ #Pulse el botón para capturar una imagen
+ if GPIO.input(Button_Pin) == True: 
+ sleep(2) 
+ print('Photo taken') 
+ camera.capture(Image,'rgb') 
+ RGB_Array = [] 
+  
+ # Para cada uno de rojo, verde y azul, calcula el color más prominente por los promedios  
+ for col in range(0,3): 
+ RGB_Array.append(np.mean(Image[:,:,col]-np.mean(Image)- np.mean(Noise[:,:,col])))  
+ Color = RGB_Text[np.argmax(RGB_Array)] 
+ print(Color) 
+  
+ # Para desafío 3, comparemos el color más prominente con el segundo más  # Podemos usar esta relación para establecer la Color_Intensity variable
+Copyright © Deloitte Development LLC 2022. All Rights Reserved. Developed in collaboration with the National Math + Science Initiative. 
+ # con max como np.max(RGB_Array) y mid como np.median(RGB_Array)  # Sin embargo, los canales de color pueden ser negativos, así que usemos un máximo para mantener el positivo 
+ Color_Intensity = np.max([np.max(RGB_Array) / np.median(RGB_Array), 2])   
+ # Para desafío 4, busquemos un patrón como Rojo y luego Color  #Podemos usar un if declaración para ver si el Last_Color era rojo  # Reemplazar está True declaración con una lógica para verificar, recuerda que es ==, not = here  if Last_Color == 'Red': 
+  
+ # Activar las salidas del controlador de motor según el color del objeto determinado  if Color == 'Red': #Hacia atrás para un objeto rojo
+ drive_backward(Backward_Time * Color_Intensity) 
+ sleep(Wait_Time) 
+  
+ if Color == 'Green': #Adelante para un objeto verde
+ drive_forward(Forward_Time * Color_Intensity) 
+ sleep(Wait_Time) 
+ if Color == 'Blue': #Gire para el objeto azul 
+  
+ if count % 2 == 0: # Trata cambiado el True declaración al módulo para desafío 2 
+ drive_left_turn(Left_Turn_Time * Color_Intensity)   
+ else: # Para desafío 2, módulo usa estos mandatos de manejo en bucles impares 
+ drive_right_turn(Right_Turn_Time * Color_Intensity)   
+ sleep(Wait_Time) 
+ count = count + 1 # Incrementa el contador para el módulo   
+ # Para desafío 4, usa Last_Color después de las salidas 
+ Last_Color = Color 
+ print('Ready to take photo') 
