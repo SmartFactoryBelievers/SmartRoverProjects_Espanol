d105e6ad9277edbf74ea20e92a66baf5d1d1b114
Smart Factory Believers <128001053+SmartFactoryBelievers@users.noreply.github.com>
2023-06-29 18:23:47 -0500

    Update Project10_ColorDetection.py

--- Project10_ColorDetection.py
+++ Project10_ColorDetection.py
@@ -1,115 +1,96 @@
-# Project 10
-# Using the Pi cameraera to capture and analyze the color profile of objects
-# Build the Project 10 circuit and indicate the color of objects with LED and buzzer
-
-#Challenge 1
-# Try swapping the LED and buzzer outputs in the code and then also on the rover
-
-#Challenge 2
-# Try writing a function to handle the LED and buzzer so it can be called after each color
-
-#Challege 3
-# Try adding a margin that the argmax for Color must exceed to be considered a certain color
-
-#Challege 4
-# Try adding a memory variable for the last color identified and activate flashes and buzzes
-# a new LED or buzzer output based on the pattern, like Red then Green
-
-#Importing libraries
-# Here we want sleep for timing, GPIO for the Pi's pins, & picamera for the Pi's camera
-from time import sleep
-import RPi.GPIO as GPIO
-from picamera import PiCamera
-# Numpy is a great numerical tools package to help with the math required
-import numpy as np
-
-#Let's define variables so we can use them later
-LED_Pin = 21 #the internal Pi pin number that goes to snap 4
-Buzzer_Pin = 26 #the internal Pi pin number that goes to snap 3
-Button_Pin = 18 #the internal Pi pin number that goes to snap 6
-#Setting up our pins
-GPIO.setmode(GPIO.BOARD)
-#Our output pins, start off
-GPIO.setup(LED_Pin, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(Buzzer_Pin, GPIO.OUT, initial=GPIO.LOW)
-#Our input pins, start down
-GPIO.setup(Button_Pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-# Setting up camera for analysis and to emphasize colors
-camera = PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 30
-sleep(2) #let the cameraera settle
-camera.iso = 100
-camera.shutter_speed = camera.exposure_speed
-camera.exposure_mode = 'off'
-gain_set = camera.awb_gains
-camera.awb_mode = 'off'
-camera.awb_gains = gain_set
-
-# Prepping for image analysis and eliminating background Noise
-#Images are stored in a 3D array with each pixel having Red, Green, and Blue values
-Image = np.empty((640,480,3),dtype=np.uint8)
-Noise = np.empty((640,480,3),dtype=np.uint8)
-RGB_Text = ['Red','Green','Blue'] #Array for naming color
-# Let's remove the background 'Noise' colors to emphasis the object's color
-camera.capture(Noise,'rgb')
-Noise = Noise-np.mean(Noise)
-
-# For challenge 2, let's create a function like we've done before for outputs
-# It should have output_pin and delay time arguments to turn them High and then Low
-#def your_function(??, ???):
-# sleep(???)
-# GPIO.output(??, GPIO.?)
-# sleep(???)
-# GPIO.output(??, GPIO.?)
-
-# For challenge 3, let's set a threshold the max color must exceed
-# This will help the camera avoid mistakes in bad lighting or glare
-Col_Margin = 0.8
-# Let's check if the max * margin > mid
-# with max as np.max(RGB_Array) and mid as np.median(RGB_Array)
-
-#Looping with different images to determine object colors upon button press
-print('Ready to take photo')
-while True:
-# Press the push button to capture an image
-if GPIO.input(Button_Pin) == True:
-sleep(2)
-print('Photo taken')
-camera.capture(Image,'rgb')
-RGB_Array = []
-# For each of red, green, and blue, calculate the most prominent color through means
-for col in range(0,3):
-RGB_Array.append(np.mean(Image[:,:,col]-np.mean(Image)-np.mean(Noise[:,:,col])))
-
-# For challenge 3, replace the True with the logical statement for the margin
-if True:
-Color = RGB_Text[np.argmax(RGB_Array)]
-print(Color)
-else:
-print('No prominent color found')
-
-# For challenge 4, let's look for a pattern like Red then Color
-# We can use an if statement to see if the Last_Color was Red
-# Replace this True with a logical to check, remember it's ==, not = here
-if True:
-# Activate outputs based on the determined object color
-if Color == 'Red': #LED for Red object
-GPIO.output(LED_Pin, GPIO.HIGH) #LED on
-sleep(2)
-GPIO.output(LED_Pin, GPIO.LOW) #LED off
-if Color == 'Green': #Buzzer for Green object
-GPIO.output(Buzzer_Pin, GPIO.HIGH) #Buzzer on
-sleep(2)
-GPIO.output(Buzzer_Pin, GPIO.LOW) #Buzzer off
-if Color == 'Blue': #LED and Buzzer for Blue object
-GPIO.output(LED_Pin, GPIO.HIGH) #LED on
-GPIO.output(Buzzer_Pin, GPIO.HIGH) #Buzzer on
-sleep(2)
-GPIO.output(LED_Pin, GPIO.LOW) #LED off
-GPIO.output(Buzzer_Pin, GPIO.LOW) #Buzzer off
-
-# For challenge 4, update Last_Color after outputs
-#Last_Color = Color
-print('Ready to take photo')
+# Proyecto 10
+# Usando las cámaras del Pi para capturar y analizar los colores de los objetos # Construya el circuito Proyecto 10 e indica el color de los objetos con LED y el zumbador 
+#Desafío 1 
+# Trate de intercambiar las salidas de LED y el zumbador en el código y luego también en el Smart Rover
+#Desafío 2 
+# Trate de escribir una función para manejar el LED y el zumbador para que pueda llamarse después de cada color 
+#Desafío 3 
+# Trate de agregar un margen para el argmax para Color debe exceder para ser considerado un cierto color 
+#Desafío 4 
+# Trate de agregar una variable de memoria para el último color identificado
+# una nueva salida de LED o zumbador tiene un patrón, como rojo y luego verde 
+#Importing libraries 
+# Aquí queremos sleep para el tiempo, GPIO para los pines de Pi y picamera para la cámara de Pi. 
+ from time import sleep 
+import RPi.GPIO as GPIO 
+from picamera import PiCamera 
+# Numpy es un gran paquete de herramientas numéricas para ayudar con las matemáticas import numpy as np 
+#Definamos variables para poder usarlas más tarde 
+LED_Pin = 35 #el número de pin interno de Pi que se ajusta a 4 
+Buzzer_Pin = 12 #el número de pin interno de Pi que se ajusta a 3 
+Button_Pin = 38 #el número de pin interno de Pi que se ajusta a 6 
+#Configurando nuestros pines
+GPIO.setmode(GPIO.BOARD) 
+#Nuestros pines de salida, empiezan apagado  
+GPIO.setup(LED_Pin, GPIO.OUT, initial=GPIO.LOW) 
+GPIO.setup(Buzzer_Pin, GPIO.OUT, initial=GPIO.LOW) 
+#Nuestros pines de entrada, empiezan abajo
+GPIO.setup(Button_Pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
+# Configuración de la cámara para el análisis y para enfatizar los colores
+camera = PiCamera() 
+camera.resolution = (640, 480)camera.framerate = 30 
+sleep(2) #deja que la cámara se asiente
+Copyright © Deloitte Development LLC 2022. All Rights Reserved. Developed in collaboration with the National Math + Science Initiative. 
+camera.iso = 100 
+camera.shutter_speed = camera.exposure_speed 
+camera.exposure_mode = 'off' 
+gain_set = camera.awb_gains 
+camera.awb_mode = 'off' 
+camera.awb_gains = gain_set 
+# Preparación para el análisis de imágenes y eliminación del ruido
+#Las imágenes se almacenan en una matriz 3D y cada píxel tiene valores de rojo, verde y azul Image = np.empty((640,480,3),dtype=np.uint8) 
+Noise = np.empty((640,480,3),dtype=np.uint8) 
+RGB_Text = ['Red','Green','Blue'] #Matriz para nombrar colores 
+# Quitando el ruido camera.capture(Noise,'rgb') 
+Noise = Noise-np.mean(Noise) 
+# Para desafío 2, creemos una función como la que hemos hecho antes para las salidas # Deber tener output_pin y argumentos de tiempo para convertirlos a High y después Low def your_function(output_pin, delay): 
+ sleep(delay) 
+ GPIO.output(output_pin, GPIO.HIGH) 
+ sleep(delay) 
+ GPIO.output(output_pin, GPIO.LOW) 
+# Para desafío 3, establezcamos un umbral que el color máximo debe superar # Esto ayudará a la cámara a evitar errores Col_Margin = 0.8 
+# Verifique que max * margin > mid 
+# con max como np.max(RGB_Array) y mid como np.median(RGB_Array) 
+#Bucle con diferentes imágenes para determinar los colores de los objetos al presionar el botón print('Ready to take photo') 
+while True: 
+  
+ # Pulse el botón para capturar una imagen 
+ if GPIO.input(Button_Pin) == True: 
+ sleep(2) 
+ print('Photo taken') 
+ camera.capture(Image,'rgb') 
+ RGB_Array = [] 
+  
+ # Para cada uno de rojo, verde y azul, calcula el color más prominente por los promedios   
+ for col in range(0,3): 
+ RGB_Array.append(np.mean(Image[:,:,col]-np.mean(Image)- np.mean(Noise[:,:,col]))) 
+ 
+Copyright © Deloitte Development LLC 2022. All Rights Reserved. Developed in collaboration with the National Math + Science Initiative. 
+ # Para desafío 3, reemplace la True declaración con la declaración lógica para el margen 
+ if np.max(RGB_Array) * Col_Margin > np.median(RGB_Array):  Color = RGB_Text[np.argmax(RGB_Array)] 
+ print(Color) 
+ else: 
+ print('No prominent color found') 
+  
+ # Para desafío 4, busquemos un patrón como Rojo y luego Color  # Podemos usar el condicional if para ver si el Last_Color era Rojo  # Reemplace está declaración True con un lógico para verificar, recuerda que es ==, no =  if Last_Color == 'Red': 
+  
+ # Activar salidas if Color == 'Red': #LED para objeto rojo 
+ GPIO.output(LED_Pin, GPIO.HIGH) #LED encendida 
+ sleep(2) 
+ GPIO.output(LED_Pin, GPIO.LOW) #LED apagado 
+  
+ if Color == 'Green': #Zumbador para objeto verde 
+ GPIO.output(Buzzer_Pin, GPIO.HIGH) #Zumbador encendida
+ sleep(2) 
+ GPIO.output(Buzzer_Pin, GPIO.LOW) #Zumbador apagado 
+  
+ if Color == 'Blue': #LED y Zumbador para objeto azul
+ GPIO.output(LED_Pin, GPIO.HIGH) #LED encendida 
+ GPIO.output(Buzzer_Pin, GPIO.HIGH) #Zumbador encendida 
+ sleep(2) 
+ GPIO.output(LED_Pin, GPIO.LOW) #LED apagado 
+ GPIO.output(Buzzer_Pin, GPIO.LOW) #Zumbador apagado 
+  
+ # Para desafío 4, actualice Last_Color después de las salidas 
+ Last_Color = Color 
+ print('Ready to take photo') 
